354fa9aa1ee11c5845ceb1ba7b8335bdf58ee1d7
Swadhin Ghosh <43653792+Swadhincse@users.noreply.github.com>
2023-03-27 00:04:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 //your JS code here. If required.
-function delayedHello() {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve("Hello, world!");
-    };
-  });
-}
+function sayHello() {
+        return new Promise(resolve => {
+          setTimeout(() => {
+            resolve("Hello, world!");
+          }, 1000);
+        });
+      }
+
+      sayHello().then(message => {
+        const outputDiv = document.getElementById("output");
+        outputDiv.textContent = message;
+      });
